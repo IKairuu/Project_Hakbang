296103393e644cff8006d6f7bd52b08be55e903a
Kim <176278148+Shalamanter@users.noreply.github.com>
2026-04-11 22:14:09 +0800

    Add container design for sign up step 2 options
    Container designs are now in the container_design to declutter code.

--- lib/design/container_design.dart
+++ lib/design/container_design.dart
@@ -27,4 +27,45 @@ class ContainerDesign {
     color: Color(0xFF1c1e27),
     borderRadius: BorderRadius.circular(20),
   );
+
+  static final BoxDecoration signupSelectionOption = BoxDecoration(
+    color: Color(0xFF2a2d38),
+    borderRadius: BorderRadius.circular(12),
+    border: Border.all(
+      color: Color.fromARGB(255, 60, 61, 67),
+      width: 1,
+    ),
+    boxShadow: [
+      BoxShadow(
+        color: Color(0xFFC8FF4D).withValues(alpha: 0.15),
+        blurRadius: 15,
+        spreadRadius: 3,
+      ),
+    ],
+  );
+
+  static final BoxDecoration signupSelectionOptionSelected = BoxDecoration(
+    color: Color.fromARGB(105, 198, 255, 75),
+    borderRadius: BorderRadius.circular(12),
+    border: Border.all(
+      color: Color(0xFFC8FF4D),
+      width: 2,
+    ),
+    boxShadow: [
+      BoxShadow(
+        color: Color(0xFFC8FF4D).withValues(alpha: 0.15),
+        blurRadius: 15,
+        spreadRadius: 3,
+      ),
+    ],
+  );
+
+  static final BoxDecoration signupIdentityOptionUnselected = BoxDecoration(
+    color: Color(0xFF2a2d38),
+    borderRadius: BorderRadius.circular(12),
+    border: Border.all(
+      color: Color.fromARGB(255, 60, 61, 67),
+      width: 1,
+    ),
+  );
 }

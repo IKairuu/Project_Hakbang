cb2c19b384e03b6fda38d337b68a91f7d4d165b9
Kairu <abandiakyle2112@gmail.com>
2026-04-16 20:54:51 +0800

    Removed unnecessary code

--- lib/functions/filter.dart
+++ lib/functions/filter.dart
@@ -1,8 +1,6 @@
 import 'package:hakbang/functions/initialization.dart';
-import 'package:hakbang/models/ai_message.dart';
 import 'package:hakbang/models/college.dart';
 import 'package:hakbang/notifiers.dart';
-import 'package:hakbang/server/services/ai_chat.dart';
 
 class Filter {
   static void filterCollegeSection(List<College> colleges) {

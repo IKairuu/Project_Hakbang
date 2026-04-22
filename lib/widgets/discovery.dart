f4271ba421e6489a0e80832757254df07bf649a5
Kairu <abandiakyle2112@gmail.com>
2026-04-22 21:52:58 +0800

    Changed available schools text color into text secondary

--- lib/widgets/discovery.dart
+++ lib/widgets/discovery.dart
@@ -253,7 +253,7 @@ class _DiscoveryState extends State<Discovery> {
                                 Text(
                                   "${univ.isEmpty ? "No" : univ.length} Available ${univ.length <= 1 ? "School" : "Schools"}",
                                   style: GoogleFonts.inter(
-                                    color: Colors.white,
+                                    color: AppColors.textSecondary,
                                     fontWeight: FontWeight.w700,
                                     fontSize: 15,
                                     letterSpacing: -0.3,
